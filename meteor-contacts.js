104a29173d2ddccb283e4b0ccb3121a2bee260b0
Zachary Ngo <zach@poeticsystems.com>
2015-04-10 21:16:13 -0500

    use router for collection instead of template helper

--- meteor-contacts.js
+++ meteor-contacts.js
@@ -1,24 +1,10 @@
-Contacts = new Mongo.Collection("contacts");
-
-if (Meteor.isClient) {
-  // client code goes here
-  Template.contacts.helpers({
-    contacts: function() {
-      return Contacts.find({});
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  // server code goes here
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
-
-Router.route('/', function () {
+Router.route('/', function() {
   this.layout('ApplicationLayout');
   this.render('contacts', {
-    data: function() { return Contacts.find({}); }
+    data: {
+      contacts: function() {
+        return Contacts.find({});
+      }
+    }
   });
 });
