f68ed62f3dd82a9aa570a51533167f5b637fb09d
Matthew Hager <matthew@poeticsystems.com>
2015-04-14 02:11:52 -0500

    Add url parameter scrubber. Gotta keep that location clean!

--- client/components/iron-modal/iron-modal.js
+++ client/components/iron-modal/iron-modal.js
@@ -17,11 +17,38 @@ Router.onBeforeAction('ironModal');
 Template.ironModal.rendered = function() {
   $(this.firstNode).openModal({
     complete: function() {
-      Router.go('/admin');
+      var location = Router.current().location.get().path;
+      var newLocation = removeURLParam(location, 'modal');
+      Router.go(newLocation);
     }
   });
 }
 
 Template.ironModal.destroyed = function() {
   $(this.firstNode).closeModal();
+}
+
+function removeURLParam (url, param) {
+    var urlparts = url.split('?');
+    if (urlparts.length >= 2) {
+        var prefix = encodeURIComponent(param) + '=';
+        var pars = urlparts[1].split(/[&;]/g);
+
+        // reverse iteration as may be destructive
+        for (var i = pars.length; i-- > 0;) {
+            // idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+                pars.splice(i, 1);
+            }
+        }
+
+        url = urlparts[0];
+        if (pars.length > 0) {
+          url += '?'+pars.join('&');
+        }
+
+        return url;
+    } else {
+        return url;
+    }
 }
